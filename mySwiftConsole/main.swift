f648d3a218295c39d9411316016941cc78281965
Daigoh Hattori <ramenplease@gmail.com>
2016-11-22 19:51:09 +0900

    Edit

---
+++ mySwiftConsole/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  mySwiftConsole
+//
+//  Created by daigoh on 2016/11/22.
+//  Copyright © 2016年 touhu. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+print("Hello, MyWorld!")
+print("Hello, MyWorld!Again")
